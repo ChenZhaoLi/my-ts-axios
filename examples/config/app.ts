8f665d3819aeb2f48712d96150bf58eb3c206617
ChenZhaoLi <49938285+ChenZhaoLi@users.noreply.github.com>
2021-06-15 00:03:43 +0800

    axios.create静态接口

--- examples/config/app.ts
+++ examples/config/app.ts
@@ -34,3 +34,24 @@ axios({
 }).then((res) => {
     console.log(res.data, 1)
 })
+const instance = axios.create({
+    transformRequest: [(function (data) {
+        return qs.stringify(data)
+    }), ...(axios.defaults.transformRequest as AxiosTransformer[])],
+    transformResponse: [...(axios.defaults.transformResponse as AxiosTransformer[]), function (data) {
+        if (typeof data === 'object') {
+            data.b = 2
+        }
+        return data
+    }]
+})
+
+instance({
+    url: '/config/post',
+    method: 'post',
+    data: {
+        a: 1
+    }
+}).then((res) => {
+    console.log(res.data)
+})
--- src/types/index.ts
+++ src/types/index.ts
@@ -67,6 +67,10 @@ export interface AxiosInstance extends Axios {
     <T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
 }
 
+export interface AxiosStatic extends AxiosInstance {
+    create(config?: AxiosRequestConfig): AxiosInstance
+}
+
 export interface AxiosPromise<T = any> extends Promise<AxiosResponse<T>> { }
 
 
